cd413ee3c5c6c21e0207dd59511ea0d607989dd0
JackPenhale <jackpenhale@gmail.com>
2023-07-16 13:25:14 -0600

    responsive changes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import bg from "./images/bg.svg";
 function App() {
   return (
     <div className="h-screen w-screen bg-cover bg-center bg-[#EFF0ED]">
-      <div className="h-3/6 bg-[url('./images/header.svg')] bg-cover">
+      <div className="h-1/3 md:h-3/6 bg-[url('./images/header.svg')] bg-cover">
         <nav className="h-14 flex flex-col w-screen p-14  items-center justify-between sm:flex-row ">
           <div className="flex">
             <img className="w-20 " src={logo} />
@@ -42,37 +42,37 @@ function App() {
 
       <div className="w-screen h-1/6 bg-[#EFF0ED] overflow-hidden flex justify-center items-center align-middle">
         <img
-          className=" mt-10 sm:-mt-56 absolute z-10 max-h-full  w-screen max-w-5xl overflow-hidden"
+          className=" mt-24 sm:-mt-56 absolute z-10 max-h-full  w-screen max-w-5xl overflow-hidden"
           src={vroom}
         />
       </div>
 
-      <div className="h-2/6 flex flex-col sm:flex-row">
+      <div className="h-2/6 flex flex-col  sm:flex-row">
         <div className="w-full sm:w-2/5">
-          <img src={bottomImage} className="h-2/6 absolute z-10" />
-          <h3 className=" absolute z-20 text-yellow-500 text-3xl pl-16 font-bold pt-10">
+          <img src={bottomImage} className="h-2/6 absolute hidden md:flex z-10" />
+          <h3 className=" md:absolute z-20 text-yellow-500 text-3xl pl-16 font-bold pt-10">
             {" "}
             LUMINANCE SOL GT
           </h3>
-          <p className=" absolute z-20 text-gray-600 w-4/5 sm:w-1/4 pl-16 pt-20">
+          <p className=" md:absolute z-20 text-grey-500 md:text-gray-300 w-full pl-6 sm:w-3/6 md:w-1/4 lg:pl-16 pt-10 md:pt-32 md:text-xl">
             Equipped with a powerful engine, advanced suspension, and
             cutting-edge technology features, the Luminance Sol GT delivers an
             unmatched driving experience that is both exhilarating and
             effortless.
           </p>
         </div>
-        <div className="pt-80 sm:w-3/5 flex sm:pt-10 items-center align-middle flex-col bg-[#EFF0ED] ">
-          <h3 className="pb-10 text-3xl font-bold">OUR SOL</h3>
+        <div className="pt-10 sm:w-3/5 flex sm:pt-10 items-center align-middle flex-col bg-[#EFF0ED] ">
+          <h3 className="pb-5 md:pb-10 text-3xl font-bold">OUR SOL</h3>
 
-          <div className="p-5 flex flex-col sm:flex-row">
+          <div className="p-5 flex flex-col lg:flex-row z-50">
             <a
-              className="border-2 border-slate-200 hover:bg-slate-200 text-black py-7 px-24 rounded-lg m-4"
+              className="border-2 bg-[#EFF0ED] border-slate-200 hover:bg-slate-200 text-black py-7 px-24 rounded-lg m-4"
               href=""
             >
               The Engine
             </a>
             <a
-              className="border-2 border-slate-200 hover:bg-slate-200 text-black py-7 px-24 rounded-lg m-4"
+              className="border-2 bg-[#EFF0ED] border-slate-200 hover:bg-slate-200 text-black py-7 px-24 rounded-lg m-4"
               href=""
             >
               The Platform
